913dd0edd8c0e4221849cafee625f30c37fa6cb0
Brian Mwaura <brianndolo846@gmail.com>
2022-12-13 04:35:16 -0800

    the main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,68 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * _putchar - function to print _putchar
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet - function to print alphabet
+ */
+void print_alphabet_x10(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+int _islower(int c);
+/**
+ * _islower - prints if c is lower case
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _isalpha(int c);
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int print_sign(int n);
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0  or -1
+ */
+int _abs(int);
+/**
+ * _abs - gives absolute value
+ * @int: integer
+ * Return: integer or abs value of int
+ */
+int print_last_digit(int);
+/**
+ * print_last_last - prints digit last
+ */
+void jack_bauer(void);
+/**
+ * jack_bauer - jack bauer
+ */
+void times_table(void);
+/**
+ * times_table - time
+ */
+int add(int, int);
+/**
+ * add - adds up numbers
+ */
+void print_to_98(int n);
+/**
+ * print_to_98 - prints to 98
+ */
+void print_times_table(int n);
+/**
+ * print_times_table - prints time table
+ */
+#endif
+ 
