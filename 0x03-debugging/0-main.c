a3419eb60e6ceee53e06c7b7e5f110f9579d58ac
Brian Mwaura <brianndolo846@gmail.com>
2022-12-15 04:59:34 -0800

    main function for testing correct output

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests the positive_or_negative() function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(0);
+
+	return (0);
+
+
+}
